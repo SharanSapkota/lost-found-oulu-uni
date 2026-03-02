4bee7e037fbb9f378bf0775b0ab7a190dd01b2d5
Sharan Sapkota <sapkotarambbo@gmail.com>
2026-03-02 12:04:05 +0200

    Update prisma client

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,9 +1,17 @@
-// src/prismaClient.ts
-import { PrismaClient } from '@prisma/client'
+import "dotenv/config";
+
+import {PrismaClient} from '../src/generated/prisma/client'
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
 
 const DATABASE_URL = process.env.DATABASE_URL
 
-export const prisma = new PrismaClient({
-  adapter: 'postgresql',
-  connectionString: DATABASE_URL,
-})
+ const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
